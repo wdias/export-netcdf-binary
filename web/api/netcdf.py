79ecb8ff26db7cb337655b772509d5579645ff97
gihankarunarathne <gckarunarathne@gmail.com>
2019-05-15 18:38:34 +0400

    Send Content-Type as `x-netcdf4`

--- web/api/netcdf.py
+++ web/api/netcdf.py
@@ -12,13 +12,13 @@ ADAPTER_GRID = 'http://adapter-grid.default.svc.cluster.local'
 def get_export_netcdf_binary(timeseries_id: str, request_name: str):
     assert timeseries_id, 'Timeseries ID should be provided'
     assert request_name.endswith('.nc'), 'Request file name should ends with .nc'
-    logger.info('>> ', timeseries_id, request_name)
+    logger.info(f'>> {timeseries_id}, {request_name}')
 
     # Solution via: 1. https://stackoverflow.com/a/5166423/1461060
     # combining 2. https://stackoverflow.com/a/39217788/1461060
-    return Response(requests.get(f'{ADAPTER_GRID}/timeseries/{timeseries_id}/{request_name}', stream=True), direct_passthrough=True)
+    return Response(requests.get(f'{ADAPTER_GRID}/timeseries/{timeseries_id}/{request_name}', stream=True), direct_passthrough=True, mimetype='application/x-netcdf4')
 
-    # Solution by: ``. https://stackoverflow.com/a/16696317/1461060 
+    # Solution by: ``. https://stackoverflow.com/a/16696317/1461060
     # 2. http://flask.pocoo.org/docs/1.0/patterns/streaming/#streaming-with-context
     # def generate():
     #     with requests.get(f'{ADAPTER_GRID}/timeseries/{timeseries_id}/{request_name}', stream=True) as r:
