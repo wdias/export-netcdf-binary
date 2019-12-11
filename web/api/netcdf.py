1c8b54e86a7a59b6fbad958d854f854cbdf04db1
gihankarunarathne <gckarunarathne@gmail.com>
2019-12-11 22:25:42 +0530

    Validate and pass the start and end date time for extract grid netCDf file export

--- web/api/netcdf.py
+++ web/api/netcdf.py
@@ -1,10 +1,12 @@
 import logging
 from flask import Blueprint, request, Response, stream_with_context
 import requests
+from datetime import datetime, timedelta
 
 bp = Blueprint('ascii_grid', __name__)
 logger = logging.getLogger(__name__)
 
+DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 ADAPTER_GRID = 'http://adapter-grid.default.svc.cluster.local'
 
 
@@ -12,11 +14,20 @@ ADAPTER_GRID = 'http://adapter-grid.default.svc.cluster.local'
 def get_export_netcdf_binary(timeseries_id: str, request_name: str):
     assert timeseries_id, 'Timeseries ID should be provided'
     assert request_name.endswith('.nc'), 'Request file name should ends with .nc'
-    logger.info(f'>> {timeseries_id}, {request_name}')
+    start = request.args.get('start')
+    assert start, 'start date time should be provide'
+    start_time = datetime.strptime(start, DATE_TIME_FORMAT)
+    end = request.args.get('end')
+    if end:
+        end_time = datetime.strptime(end, DATE_TIME_FORMAT)
+    else:
+        end_time = start_time + timedelta(hours=24)
+    query_string = f"start={start}&end={end_time.strftime(DATE_TIME_FORMAT)}"
+    logger.info(f'>> {timeseries_id}, {request_name}, {query_string}')
 
     # Solution via: 1. https://stackoverflow.com/a/5166423/1461060
     # combining 2. https://stackoverflow.com/a/39217788/1461060
-    return Response(requests.get(f'{ADAPTER_GRID}/timeseries/{timeseries_id}/{request_name}', stream=True), direct_passthrough=True, mimetype='application/x-netcdf4')
+    return Response(requests.get(f'{ADAPTER_GRID}/timeseries/{timeseries_id}/{request_name}?{query_string}', stream=True), direct_passthrough=True, mimetype='application/x-netcdf4')
 
     # Solution by: ``. https://stackoverflow.com/a/16696317/1461060
     # 2. http://flask.pocoo.org/docs/1.0/patterns/streaming/#streaming-with-context
